1cdb54de770df2bd0e040be06cf30205e98ef800
an-lee1308 <lvanqbhn@gmail.com>
2023-03-21 20:30:47 +0700

    Number of Zero-Filled Subarrays

---
+++ Number of Zero-Filled Subarrays/solution.cs
@@ -0,0 +1,23 @@
+public class Solution {
+    public long ZeroFilledSubarray(int[] nums) {
+        long count = 0;
+        long result = 0;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] == 0)
+                count++;
+            else if (count > 0)
+            {
+                result += (count * (count + 1)) / 2;
+                count = 0;
+            }
+        }
+
+        if (count > 0)
+        {
+            result += (count * (count + 1)) / 2;
+        }
+
+        return result;
+    }
+}
